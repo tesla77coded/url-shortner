6a64395cfd7ee125fe28201127163de3a50d5db6
tesla77coded <kadamjaydeep60@gmail.com>
2025-09-24 21:28:13 +0530

    chore: installed necessary test packages and jest config

---
+++ jest.config.ts
@@ -0,0 +1,13 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/tests'],
+  testMatch: ['**/*.test.ts'],
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+};
